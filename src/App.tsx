fa92a5fc3e374ca9588fb26785273fa06e465023
masuyama-san <soichi_masuyama@narutech.co.jp>
2025-04-08 14:38:25 +0900

    create: amplify gen2のプロジェクトの作成

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS with Vite!</h1>
+    </div>
+  );
+}
+
+export default App;
